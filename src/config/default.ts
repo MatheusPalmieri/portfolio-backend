12baa41cd414514eb8f1e712d3de56a686d913e9
Matheus Palmieri <matheuspalmieridev@gmail.com>
2023-10-20 14:18:11 -0300

    fix: deploy

--- src/config/default.ts
+++ src/config/default.ts
@@ -2,5 +2,5 @@ export default {
   port: process.env.PORT,
   dbUri: `${process.env.MONGO_DB_URL}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
   env: process.env.TYPE_OF_ENVIRONMENT,
-  NODE_ENV: process.env.TYPE_OF_ENVIRONMENT,
+  NODE_ENV: 'production',
 };
