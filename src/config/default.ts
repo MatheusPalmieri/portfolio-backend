9d928b253e52e49eee1cad03b31965ebb7cacaeb
Matheus Palmieri <matheuspalmieridev@gmail.com>
2023-10-20 14:03:35 -0300

    fix: remove env

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,5 +1,5 @@
 export default {
   port: process.env.PORT,
   dbUri: `${process.env.MONGO_DB_URL}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
-  env: process.env.TYPE_OF_ENVIRONMENT,
+  // env: process.env.TYPE_OF_ENVIRONMENT,
 };
