ece3f439be6bfb18a4ee161a4307e534ab612505
Matheus Palmieri <matheuspalmieridev@gmail.com>
2023-10-20 15:05:40 -0300

    feat: config database

--- config/default.ts
+++ config/default.ts
@@ -1,9 +1,5 @@
-const urlDB = process.env.MONGO_DB_URL;
-const nameDB = process.env.MONGO_DB_NAME;
-const db = `${urlDB}/${nameDB}?retryWrites=true&w=majority`;
-
 export default {
-  env: 'development', // Defina 'env' para o valor padrão, como 'development'.
   port: process.env.PORT,
-  dbUri: db,
+  dbUri: `${process.env.MONGO_DB_URL}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
+  env: process.env.TYPE_OF_ENVIRONMENT,
 };
